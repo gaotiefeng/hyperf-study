d485ba8e4763e77095058412373c960e1e19d98b
高铁峰 <1096392101@qq.com>
2019-08-30 17:55:45 +0800

    update

--- config/routes/web.php
+++ config/routes/web.php
@@ -17,3 +17,10 @@ Router::get('/', 'App\Controller\IndexController::index');
 Router::post('/user/login', 'App\Controller\Web\UserController::login');
 Router::post('/user/register', 'App\Controller\Web\UserController::register');
 Router::get('/user/info', 'App\Controller\Web\UserController::userInfo');
+
+/*sudo docker run -d --hostname www.tfuu.cn \
+--publish 443:443 --publish 8880:80 --publish 22:22 \
+--name gitlab --restart always --volume /srv/gitlab/config:/etc/gitlab \
+--volume /srv/gitlab/logs:/var/log/gitlab \
+--volume /srv/gitlab/data:/var/opt/gitlab \
+gitlab/gitlab-ce:latest*/
