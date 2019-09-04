1fa7e33cd6bbef4804acf6b0859a70661c26238f
高铁峰 <1096392101@qq.com>
2019-09-04 18:19:50 +0800

    update

--- app/Amqp/Consumer/MessageConsumer.php
+++ app/Amqp/Consumer/MessageConsumer.php
@@ -7,6 +7,7 @@ namespace App\Amqp\Consumer;
 use Hyperf\Amqp\Result;
 use Hyperf\Amqp\Annotation\Consumer;
 use Hyperf\Amqp\Message\ConsumerMessage;
+use SebastianBergmann\CodeCoverage\Report\PHP;
 
 /**
  * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", name ="MessageConsumer", nums=1)
@@ -15,6 +16,8 @@ class MessageConsumer extends ConsumerMessage
 {
     public function consume($data): string
     {
+        print_r($data);
+
         return Result::ACK;
     }
 }
