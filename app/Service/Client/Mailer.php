e50aa39b687f42a16dbe68f6f4864ec9ac6a9360
高铁峰 <1096392101@qq.com>
2019-10-14 15:09:51 +0800

    updage

--- app/Service/Client/Mailer.php
+++ app/Service/Client/Mailer.php
@@ -5,6 +5,8 @@ namespace App\Service\Client;
 
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class Mailer
 {
@@ -18,19 +20,19 @@ class Mailer
     {
         $this->username = '';
         $this->name = 'liunian';
-        $this->password;
+        $this->password = '';
 
         $mail = new PHPMailer(true);
         try {
             //Server settings
             //Server settings
             $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-            // $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Mailer = 'SMTP';
             $mail->Host = 'smtp.qq.com';  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = $this->username;                 // SMTP username
-            $mail->Password = '';                           // SMTP password
+            $mail->Password = $this->password;                           // SMTP password
             $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 465;                                    // TCP port to connect to
             //Recipients
@@ -42,9 +44,9 @@ class Mailer
 
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'You are very 漂亮';
+            $mail->Body    = '<h6>I love you</h6><b>!</b>';
+            $mail->CharSet = 'UTF-8';
 
             $mail->send();
             echo 'Message has been sent';
